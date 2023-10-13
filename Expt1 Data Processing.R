fc25c0f3ede9247877c2ddb62cfdcc53bca2f794
MikaylaSchmidt <145627161+MikaylaSchmidt@users.noreply.github.com>
2023-10-13 12:28:40 +1100

    Add files via upload

---
+++ Expt1 Data Processing.R
@@ -0,0 +1,171 @@
+#Script for processing of raw data into full dataset used in analysis
+#Originally part of script 'Expt1 Analysis2'
+
+
+
+# you need to add a bit that combines the final SA of caliper for all but halimeda + halimeda
+
+
+
+#0.open script, set names and parameters
+dShellFull <- read.csv('./shells_Expt1.csv', skip=22)
+
+#remove missing and broken taxon
+dShell<- dShellFull[-c(10,38,39,77,79,91),]
+
+dateFormat <- '%m/%d/%y %H:%M'
+EXPID <- unique(dShell$exptID)
+
+#General calculations of mass lost and time
+dShell$cMass <- -1 * (dShell$mass2 - dShell$mass1)
+dShell$pMass <- (dShell$mass1 - dShell$mass2) / dShell$mass1
+dShell$cTime1 <- strptime(dShell$dateM1, format=dateFormat)
+dShell$cTime2 <- strptime(dShell$dateM2, format=dateFormat)
+dShell$cDuration <- dShell$cTime2 - dShell$cTime1
+
+#General calculation of shell size for each taxa
+dShell$cSize <- (dShell$xDim * dShell$yDim * dShell$zDim) ^ (1/3)
+
+
+
+#1.1 Surface area calculations for all of the taxa using caliper measurements
+#by default, all are classified as cylinder
+dShell$shape <- 'cylinder1'
+dShell[(dShell$taxon == 'Liloa'),'shape'] <-'cylinder2'
+dShell[(dShell$taxon == 'Aragonite'),'shape'] <- 'cube'
+dShell[(dShell$taxon == 'Tridacna'),'shape'] <- 'cube'
+
+#turbo is dome + circle aka hemisphere
+dShell[(dShell$taxon == 'Turbo'), 'shape'] <- 'hemisphere'
+
+#pingui,abra, and scaph technically two domes
+dShell[(dShell$taxon == 'Pinguitellina'), 'shape'] <- '2dome'
+dShell[(dShell$taxon == 'Abranda'), 'shape'] <- '2dome'
+dShell[(dShell$taxon == 'Scaphopod'), 'shape'] <- '2dome'
+
+#halimeda different shape entirely... diamond/rhombus
+dShell[(dShell$taxon =='Halimeda'), 'shape'] <- 'rhombus'
+
+#finally, nat and eth are spherical
+dShell[(dShell$taxon == 'Natica'), 'shape'] <-'sphere'
+dShell[(dShell$taxon == 'Ethalia'), 'shape'] <-'sphere'
+
+
+
+#1.2 surface area caliper calculations
+#basic rectangular cube surface area. does not work for halimeda based off how measurements were taken.
+#is 2xy + 2yz + 2xz
+dShell$cSA1 <- 2*(dShell$xDim * dShell$yDim) + 2*(dShell$yDim * dShell$zDim) + 2*(dShell$xDim * dShell$zDim)
+
+#next lines for surface area of cylindrical specimen, for margi cylinder
+#equal to 2piR^2 + 2*h*pi*r
+subCyl1 <- which(dShell$shape == 'cylinder1')
+dShell[subCyl1,'cSA1'] <- 2 * (pi* dShell[subCyl1,'xDim']/2 * dShell[subCyl1,'yDim']/2) + dShell[subCyl1,'zDim']*pi*(dShell[subCyl1,'yDim'] + dShell[subCyl1,'xDim'])/2
+#for liloa cylinder, 2piR^2 + 2*h*pi*r
+subCyl2 <- which(dShell$shape == 'cylinder2')
+dShell[subCyl2,'cSA1'] <- 2 * (pi* dShell[subCyl2,'yDim']/2 * dShell[subCyl2,'yDim']/2) + 2* dShell[subCyl2,'zDim']*pi*dShell[subCyl2,'yDim']/2 
+
+#next turbo, a hemisphere
+#is pi*r^2 + 2*pi*r*h + 
+subHemi <- which(dShell$shape == 'hemisphere')
+dShell[subHemi,'cSA1'] <-  pi * (dShell[subHemi,'xDim']/2 * dShell[subHemi,'yDim']/2) + 2 * pi * (dShell[subHemi,'xDim']/2 + dShell[subHemi,'yDim']/2)/2 * dShell[subHemi,'zDim'] 
+
+#next lines for surface area of domed specimens
+# is 2 * (2*pi*r*h)
+subDome <- which(dShell$shape == '2dome')
+dShell[subDome,'cSA1'] <- 2 * pi * 2 *(dShell[subDome,'xDim']/2 + dShell[subDome,'yDim']/2)/2 * dShell[subDome,'zDim']
+
+#then, rough surface area for rhomboid Halimeda
+#surface area is 2(xy/2) + 4(zc) where c = ((x/2)^2) + (y/2)^2)^1/2 
+subRhom <- which(dShell$shape == 'rhombus')
+dShell[subRhom,'cDim'] <- sqrt((dShell[subRhom,'xDim']/2)^2 + (dShell[subRhom,'yDim']/2)^2)
+dShell[subRhom,'cSA1'] <- 2 * ((dShell[subRhom,'xDim']*dShell[subRhom,'yDim'])/2) + 4 * dShell[subRhom,'zDim'] * dShell[subRhom,'cDim']
+
+#finally, nat and eth spherical calc
+#is 4 * pi * [(x/2 + y/2 + z/2)/3]^2
+subSphere <- which(dShell$shape == 'sphere')
+dShell[subSphere,'cSA1'] <- 4 * pi * ((dShell[subSphere,'xDim']/2+dShell[subSphere,'yDim']/2 + dShell[subSphere,'zDim']/2)/3)^2
+
+
+
+#1.3 General calculation of surface area for each taxa based on ImageJ calculations
+#SA for cube
+dShell$calcSA <- dShell$calcSA1 + dShell$calcSA2 + 2*(dShell$calcY * dShell$zDim) + 2*(dShell$calcX * dShell$zDim)
+#SA for cube, resolved for aragonite
+dShell$calcSA <- 2*(dShell$calcX * dShell$calcY) + 2*(dShell$calcY * dShell$zDim) + 2*(dShell$calcX * dShell$zDim)
+
+#next lines for surface area of cylindrical specimens
+#equal to 2piR^2 + 2*h*pi*r
+subCyl1 <- which(dShell$shape == 'cylinder1')
+dShell[subCyl1,'calcSA'] <- dShell[subCyl1,'calcSA1'] + dShell[subCyl1,'calcSA2'] + dShell[subCyl1,'zDim'] * pi * (dShell[subCyl1,'calcY'] + dShell[subCyl1,'calcX'])/2
+
+#liloa calc based on imageJ
+subCyl2 <- which(dShell$shape == 'cylinder2')
+dShell[subCyl2,'calcSA'] <- 2 * (pi* dShell[subCyl2,'calcY']/2 * dShell[subCyl2,'calcY']/2) + 2* dShell[subCyl2,'zDim']*pi*dShell[subCyl2,'calcY']/2 
+
+#next turbo, a hemisphere
+#equal to pi*r^2 + 2*pi*r*h
+subHemi <- which(dShell$shape == 'hemisphere')
+dShell[subHemi,'calcSA'] <-  dShell[subHemi,'calcSA1'] + 2 * pi * (dShell[subHemi,'calcX']/2 + dShell[subHemi,'calcY']/2)/2 * dShell[subHemi,'zDim'] 
+
+#next lines for surface area of domed specimens
+#calcSA1 = pi *r^2
+subDome <- which(dShell$shape == '2dome')
+dShell[subDome,'calcSA'] <- 2 * pi * 2 *(dShell[subDome,'calcX']/2 + dShell[subDome,'calcY']/2)/2 * dShell[subDome,'zDim']
+
+#then, rough surface area for rhomboid Halimeda
+#surface area is 2(xy/2) + 4(zc) where c = ((x/2)^2) + (y/2)^2)^1/2 
+subRhom <- which(dShell$shape == 'rhombus')
+dShell[subRhom,'calcC'] <- sqrt((dShell[subRhom,'calcX']/2)^2 + (dShell[subRhom,'calcY']/2)^2)
+dShell[subRhom,'calcSA'] <- dShell[subRhom,'calcSA1'] + dShell[subRhom,'calcSA2'] + 4 * dShell[subRhom,'zDim'] * dShell[subRhom,'calcC']
+
+#finally, nat and eth spherical calc
+#is 4 * pi * [(x/2 + y/2 + z/2)/3]^2
+subSphere <- which(dShell$shape == 'sphere')
+dShell[subSphere,'calcSA'] <- 4 * pi * ((dShell[subSphere,'calcX']/2+dShell[subSphere,'calcY']/2 + dShell[subSphere,'zDim']/2)/3)^2
+
+#1.4 Density as given in Kosnik et al 2009
+
+#Calculations of volume based on assigned shape
+
+#cube volume = lwh 
+dShell$volume <- dShell$xDim * dShell$yDim * dShell$zDim
+#cylinder1 (margi) = pi r2 h  
+dShell[subCyl1,'volume'] <- pi * (dShell[subCyl1, 'xDim']/2 * dShell[subCyl1, 'yDim']/2) * dShell[subCyl1, 'zDim']
+#cylinder2 (liloa) = pi r2 h
+dShell[subCyl2,'volume'] <- pi * (dShell[subCyl2, 'yDim']/2)^2 * dShell[subCyl2, 'zDim']
+#dome (turbo) = 1/3 pi h^2 (3* r-h)
+dShell[subHemi,'volume'] <- 1/3 * pi * dShell[subHemi,'zDim']^2 * (3 * (dShell[subHemi,'xDim']/2 + dShell[subHemi,'yDim']/2)/2 - dShell[subHemi,'zDim']) 
+# 2dome = dome1 full volume = 1/3 pi h^2 (3* r-h) 
+dShell[subDome,'volume'] <- 1/3 * pi * dShell[subDome,'zDim']^2 * (3 * (dShell[subDome,'xDim']/2 + dShell[subDome,'yDim']/2)/2 - dShell[subDome,'zDim'])
+#rhombus(halimeda) = l w h with length * width being = the image J calculated surface area?
+dShell[subRhom,'volume'] <- (dShell[subRhom, 'calcSA1']+dShell[subRhom, 'calcSA2'])/2 * dShell[subRhom, 'zDim']
+#sphere (nat and eth) = 4/3 pi r3
+dShell[subSphere,'volume'] <- 4/3 * pi * (dShell[subSphere, 'xDim']/2 * dShell[subSphere, 'yDim']/2 * dShell[subSphere, 'zDim']/2)
+#(mass^1/3)/volume^1/3 with shell volume substituted for shell size 
+#do youo need to take both to 1/3 sqrt?
+dShell$density <- dShell$cMass / dShell$volume
+
+
+#1.5 export this data as a csv
+write.csv(dShell, file = '/Users/micke/OneDrive/Desktop/shellsData_Expt1.csv', row.names=TRUE)
+
+
+
+#2.1 
+pData <- dShell
+TAXA <- unique(pData$taxon)
+
+pData$taxon <- as.factor(pData$taxon)
+taxa <- sort(unique(pData$taxon))
+tFont <- rep(3,length(taxa))
+tFont[which(taxa == 'Aragonite')] <- 1
+taxaAbrev <- substring(taxa,0,5)
+
+#this has to come AFTER THE ABOVE
+rep1.1 <- subset(pData,exptID =='T1.1')
+rep1.2 <- subset(pData,exptID =='T1.2')
+rep1.3 <- subset(pData,exptID =='T1.3')
+
+
+
---
+++ Expt1 Post Processing Analysis.R
@@ -0,0 +1,162 @@
+#Script for analysis of cleaned data after processing
+#Originally part of script 'Expt1 Analysis2'
+
+dShell <- read.csv('./shellsData_Expt1.csv')
+
+#1.1 Comparison of the Caliper vs ImageJ measurements
+#Setup for measurement comp  by taxa
+pData <- dShell
+TAXA <- unique(pData$taxon)
+pData$taxon <- as.factor(pData$taxon)
+taxa <- sort(unique(pData$taxon))
+tFont <- rep(3,length(taxa))
+tFont[which(taxa == 'Aragonite')] <- 1
+taxaAbrev <- substring(taxa,0,5)
+
+#1.2 Plot of caliper SA / ImageJ SA with abline at 1
+plot(cSA1/calcSA ~ taxon, data=pData, ann=FALSE, axes=FALSE, ylab='', xlim=c(0,11))
+points(cSA1/calcSA ~ taxon, data=pData)
+abline(a=1, b=0)
+mtext('Caliper SA / Calculated SA', side=2, line=3)
+axis(2, las=1)
+axis(1, at=1:length(taxa), labels=taxaAbrev, font=tFont, cex=0.5, las=2)
+
+#1.3 Log plot of SAs
+pData2 <- pData[!is.na(pData$calcSA),]
+plot(cSA1~calcSA, data=pData2, pch=substring(pData2$taxon, 0, 2), log='xy', xlab='log(ImageJSA)', ylab='log(CaliperSA)')
+abline(a=0, b=1)
+levels(pData$taxon)
+
+#1.4 Log plot of SAs by taxon
+plot((cSA1-calcSA)/calcSA~taxon, data=pData2, pch=substring(pData$taxon, 0, 2))
+log2(((pData2$cSA1-pData2$calcSA)/pData2$calcSA)+1)
+
+#1.5 Mann-Whitney U Tests of each taxon (for ImageJ to caliper measures)
+#Subset all 11 taxa for tests
+
+Abra <- subset(dShell, taxon =='Abranda')
+Abra1<-wilcox.test(Abra$cSA1, Abra$calcSA, data=dShell)
+print(Abra1)
+
+Arag <- subset(dShell, taxon =='Aragonite')
+Arag1<-wilcox.test(Arag$cSA1, Arag$calcSA, data=dShell)
+print(Arag1)
+
+Eth <- subset(dShell, taxon =='Ethalia')
+Eth1<-wilcox.test(Eth$cSA1, Eth$calcSA, data=dShell)
+print(Eth1)
+
+Hali <- subset(dShell, taxon =='Halimeda')
+Hali1<-wilcox.test(Hali$cSA1, Hali$calcSA, data=dShell)
+print(Hali1)
+
+Liloa <- subset(dShell, taxon =='Liloa')
+Liloa1<-wilcox.test(Liloa$cSA1, Liloa$calcSA, data=dShell)
+print(Liloa1)
+
+Margi <- subset(dShell, taxon =='Marginopora')
+Margi1<-wilcox.test(Margi$cSA1, Margi$calcSA, data=dShell)
+print(Margi1)
+
+Nat <- subset(dShell, taxon =='Natica')
+Nat1<-wilcox.test(Nat$cSA1, Nat$calcSA, data=dShell)
+print(Nat1)
+
+Pingui <- subset(dShell, taxon =='Pinguitellina')
+Pingui1<-wilcox.test(Pingui$cSA1, Pingui$calcSA, data=dShell)
+print(Pingui1)
+
+Scaph <- subset(dShell, taxon =='Scaphopod')
+Scaph1<-wilcox.test(Scaph$cSA1, Scaph$calcSA, data=dShell)
+print(Scaph1)
+
+Tri <- subset(dShell, taxon =='Tridacna')
+Tri1<-wilcox.test(Tri$cSA1, Tri$calcSA, data=dShell)
+print(Tri1)
+
+Turbo <- subset(dShell, taxon =='Turbo')
+Turbo1<-wilcox.test(Turbo$cSA1, Turbo$calcSA, data=dShell)
+print(Turbo1)
+
+
+#2.1 Dissolution (mg) relationship to surface area (mm^2)
+#need to separate taxon out by color rather than letter
+
+plot(cMass ~ cSA1, data=dShell, xlab='Surface Area (mm\u00b2)', ylab='Dissolution (mg)',pch=substring(dShell$taxon, 0, 2))
+fit <- glm(cMass~cSA1, data=dShell)
+co <- coef(fit)
+abline(fit, lwd=2)
+
+
+#2.2 second, plot the mass vs calc by taxon
+
+plot(cMass/calcSA ~ taxon, data=pData, ann=FALSE, axes=FALSE, ylab='')
+points(cMass/calcSA ~ taxon, data=pData)
+mtext('Mass lost / Calculated SA', side=2, line=3)
+axis(2, las=1)
+axis(1, at=1:length(taxa), labels=taxa, font=tFont, cex=0.5, las=2)
+
+
+
+#2.3 Modeling this all - start with basic linear models
+#Do you need to change pMass (%) to cMass (total in mg) 
+a <- lm(pMass ~ calcSA, data=pData)
+summary(a)
+b <- lm(pMass ~ calcSA + mass1, data=pData)
+summary(b)
+c <- lm(pMass ~ calcSA + taxon, data=pData)
+summary(c)
+d <- lm(pMass ~ calcSA + mass1 + taxon, data=pData)
+summary(d)
+e <- lm(pMass ~ calcSA + density, data=pData)
+summary(e)
+f <- lm(pMass ~ calcSA + mass1 + density, data=pData)
+summary(f)
+g <- lm(pMass ~ calcSA + mass1 + taxon + density, data=pData)
+summary(g)
+
+a$residuals
+
+#don't forget to add model looking at replicates
+
+
+
+
+#3.1 Comparison of the pH over time 
+#read in data
+pH1.1 <- read.csv('./Expt 1.1 Log 1.csv')
+pH1.2 <- read.csv('./Expt 1.2 Log 1.csv')
+pH1.3 <- read.csv('./Expt 1.3 Log 1.csv')
+
+#convert interval to hours for each (interval # x 5 min / 60)
+pH1.1$hours <- (pH1.1$Interval * 5)/60 
+pH1.2$hours <- (pH1.2$Interval * 5)/60 
+pH1.3$hours <- (pH1.3$Interval * 5)/60 
+
+#time ~ pH for replicate 1.1
+plot(pH ~ hours, data=pH1.1, type = "o", xlab = "Time (hours)", ylab = "pH", main = "Replicate 1.1 pH Over Time")
+fit1.1 <- glm(pH ~ hours, data=pH1.1)
+co <- coef(fit1.1)
+abline(fit1.1, lwd=2)
+
+#repeat for 1.2
+plot(pH ~ hours, data=pH1.2, type = "o", xlab = "Time (hours)", ylab = "pH", main = "Replicate 1.2 pH Over Time")
+fit1.2 <- glm(pH ~ hours, data=pH1.2)
+co <- coef(fit1.2)
+abline(fit1.2, lwd=2)
+
+#and 1.3
+plot(pH ~ hours, data=pH1.3, type = "o", xlab = "Time (hours)", ylab = "pH", main = "Replicate 1.3 pH Over Time")
+fit1.3 <- glm(pH ~ hours, data=pH1.3)
+co <- coef(fit1.3)
+abline(fit1.3, lwd=2)
+
+dev.off()
+
+#3.2 Examining pH 
+pH1 <- lm(pH ~ hours, data=pH1.1)
+summary(pH1)
+pH2 <- lm(pH ~ hours, data=pH1.2)
+summary(pH2)
+pH3 <- lm(pH ~ hours, data=pH1.3)
+summary(pH3)
